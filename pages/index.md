ffd14d0ea7d372acbaa3049d3ddf390bb6950f2a
jokerlin <anonymous.joker.lin@gmail.com>
2024-04-01 09:49:54 +0800

    chore: add lint and adjust about page

--- pages/index.md
+++ pages/index.md
@@ -6,19 +6,11 @@ title: jokerlin
   <Plum/>
 </ClientOnly>
 
-你好，我是 jokerlin，目前在字节跳动工作。
-
-我的专长在于人工智能代理、大型语言模型(LLM)评估以及 API 优先的开发策略。
-
-除了我的专业工作之外，我还对扑克和多语言学习充满热情。
-
-*** 
-
 Hey, I am jokerlin, currently working @ByteDance. 
 
 My expertise lies in Artificial Intelligence agents, Large Language Model (LLM) evaluation, API-first development strategies.
 
-Beyond my professional endeavors, I have a passion for poker and the study of multiple languages.
+Beyond my professional endeavors, I have a passion for poker, productivity and the study of multiple languages.
 
 ***
 
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,12 +1,12 @@
+<script setup>
+import { ref } from 'vue'
+
+const currentYear = ref(new Date().getFullYear())
+
+</script>
 <template>
   <div class="mt-10 mb-6 prose m-auto opacity-50 flex">
-    <span class="text-sm"><a target="_blank" href="https://creativecommons.org/licenses/by-nc-sa/4.0/" style="color:inherit">CC BY-NC-SA 4.0</a> {{currentYear}} © jokerlin </span>
+    <span class="text-sm"><a target="_blank" href="https://creativecommons.org/licenses/by-nc-sa/4.0/" style="color:inherit">CC BY-NC-SA 4.0</a> {{ currentYear }} © jokerlin </span>
     <div class="flex-auto" />
   </div>
 </template>
-<script setup>
-import { ref } from 'vue';
-
-const currentYear = ref(new Date().getFullYear());
-
-</script>
